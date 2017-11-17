0e73fc421c72be4c2f8c2d3d6437e54059803652
Alexander Larin <ekzebox@gmail.com>
2017-11-17 12:48:45 +0300

    api little refactoring

--- core/api.js
+++ core/api.js
@@ -7,38 +7,35 @@ export class Api {
     constructor() {
     }
 
-    cover(id) { return `http://coverartarchive.org/release/${id}/front`; }
-
-    searchAlbums(title, limit, offset) {
-        return new Promise((resolve, reject) => {
-                Client
-                    .get('http://musicbrainz.org/ws/2/release/')
-                    .query({ fmt: 'json', limit, offset })
-                    .query(`query=release:${encodeURIComponent(title)}`)
-                    .end((err, res) => {
-                        if (!err && res.ok)
-                            return resolve(res.body);
-                        return reject(new HttpError(err.status, res ? (res.body && res.body.error) : err.message));
-                    });
-            })
-            .then((response) => ({
-                count: response.count,
-                items: response.releases.map(({ id, ...item }) => ({ id, cover: { image: this.cover(id) }, ...item }))
-            }));
+    cover({ id, ...item }) {
+        return { id, cover: { image: `http://coverartarchive.org/release/${id}/front` }, ...item };
     }
 
-    getAlbum(id) {
+    get(query, id) {
         return new Promise((resolve, reject) => {
             Client
-                .get(`http://musicbrainz.org/ws/2/release/${id}`)
+                .get(`http://musicbrainz.org/ws/2/release/${id || ''}`)
                 .query({ fmt: 'json' })
-                .query(`inc=artist-credits+recordings`)
+                .query(query)
                 .end((err, res) => {
                     if (!err && res.ok)
                         return resolve(res.body);
                     return reject(new HttpError(err.status, res ? (res.body && res.body.error) : err.message));
                 });
-            })
-            .then(({ id, ...item }) => ({ item: { id, cover: { image: this.cover(id) }, ...item } }));
+        });
+    }
+
+
+    getAlbum(id) {
+        return this.get(`inc=artist-credits+recordings`, id)
+            .then((item) => ({ item: this.cover(item) }));
+    }
+
+    searchAlbums(title, limit, offset) {
+        return this.get(`limit=${limit}&offset=${offset}&query=release:${encodeURIComponent(title)}`)
+            .then(({ count, releases }) => ({
+                count,
+                items: releases.map((item) => this.cover(item))
+            }));
     }
 }
